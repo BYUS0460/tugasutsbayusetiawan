3f3c1512369d0bb500f0721212a6caccd92b6074
BYUS0460 <92735091+BYUS0460@users.noreply.github.com>
2024-08-06 21:19:43 +0700

    first init github

---
+++ lab4_php_modular/about.php
@@ -0,0 +1,6 @@
+<?php require('header.php'); ?>
+<div class="content">
+    <h2>About</h2>
+    <p> bagian content dari halaman.</p>
+</div>
+<?php require('footer.php'); ?>
---
+++ lab4_php_modular/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>LATIHAN HEADER</title>
+    <link href="style.css" rel="stylesheet" type="text/stylesheet" media="screen" />
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Percobaan Modularisasi</h1>
+        </header>
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="about.php">Tentang</a>
+            <a href="kontak.php">Kontak</a>
+        </nav>
